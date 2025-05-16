2303f28bae57eea8d552fee77700ed70bc757dc6
megahomyak <g.megahomyak@gmail.com>
2025-05-16 18:46:03 +0500

    Wrote down quite a bunch of different, possibly good ideas and thoughts

--- 3vcs.sh
+++ 3vcs.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-end() {
-    echo "$1" >&2
-    exit 1
-}
-
-COMMAND="$1"
-if [ "$COMMAND" = '' ]; then
-    end 'No command name entered, exiting'
-elif [ "$COMMAND" = 'mv' ]; then
-
-elif [ "$COMMAND" = 'goto' ]; then
-elif [ "$COMMAND" = 'init' ]; then
-elif [ "$COMMAND" = 'fork' ]; then
-elif [ "$COMMAND" = 'rm' ]; then
-
-else
-    end 'Invalid command name'
-fi
+# TODO
